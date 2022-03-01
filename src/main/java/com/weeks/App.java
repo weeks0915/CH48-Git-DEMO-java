d9ae86ae3809f0a3c1113d4a882ec6d87108e2e5
weeks <947390846@qq.com>
2022-03-01 18:01:55 +0800

    111

---
+++ src/main/java/com/weeks/App.java
@@ -0,0 +1,13 @@
+package com.weeks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
